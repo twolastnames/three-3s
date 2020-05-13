2e6d66381630482c989c97536c2edf06354e9a8e
Alan Glenn <alanglenn74@gmail.com>
2020-05-13 21:12:45 +0000

    feat: change project name in backend

--- backend/spec/requests/suites_request_spec.rb
+++ backend/spec/requests/suites_request_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe 'Suites', type: :request do
   end
 
   it 'can retreive an empty index' do
-    get '/three3/suites'
+    get '/threAS3/suites'
     expect_body(offset: 0, suites: [])
   end
 
   it 'can retreive an index with a value' do
     Suite.create(name: 'my suite')
-    get '/three3/suites'
+    get '/threAS3/suites'
     expect_body(
       offset: 0,
       suites: [{ name: 'my suite' }]
@@ -29,22 +29,22 @@ RSpec.describe 'Suites', type: :request do
   end
 
   it '400s with non integer offset' do
-    get '/three3/suites', params: { offset: 'hello' }
+    get '/threAS3/suites', params: { offset: 'hello' }
     expect(response).to have_http_status(400)
   end
 
   it '400s with non integer limit' do
-    get '/three3/suites', params: { limit: 'world' }
+    get '/threAS3/suites', params: { limit: 'world' }
     expect(response).to have_http_status(400)
   end
 
   it '400s with negative offset' do
-    get '/three3/suites', params: { offset: '-7' }
+    get '/threAS3/suites', params: { offset: '-7' }
     expect(response).to have_http_status(400)
   end
 
   it '400s with negative limit' do
-    get '/three3/suites', params: { limit: -5 }
+    get '/threAS3/suites', params: { limit: -5 }
     expect(response).to have_http_status(400)
   end
 
@@ -57,7 +57,7 @@ RSpec.describe 'Suites', type: :request do
     end
 
     it 'can retreive a all values' do
-      get '/three3/suites'
+      get '/threAS3/suites'
       expect_body(
         offset: 0,
         suites: [
@@ -69,7 +69,7 @@ RSpec.describe 'Suites', type: :request do
     end
 
     it 'will obey an offset' do
-      get '/three3/suites', params: { offset: 1 }
+      get '/threAS3/suites', params: { offset: 1 }
       expect_body(
         offset: 1,
         suites: [
@@ -80,7 +80,7 @@ RSpec.describe 'Suites', type: :request do
     end
 
     it 'will obey a limit' do
-      get '/three3/suites', params: { limit: 2 }
+      get '/threAS3/suites', params: { limit: 2 }
       expect_body(
         offset: 0,
         suites: [
@@ -91,7 +91,7 @@ RSpec.describe 'Suites', type: :request do
     end
 
     it 'will obey a limit and offset' do
-      get '/three3/suites', params: { limit: 1, offset: 1 }
+      get '/threAS3/suites', params: { limit: 1, offset: 1 }
       expect_body(
         offset: 1,
         suites: [
@@ -107,33 +107,33 @@ RSpec.describe 'Suites', type: :request do
     end
 
     it 'can create a suite' do
-      post '/three3/suites', params: { name: 'created suite' }
+      post '/threAS3/suites', params: { name: 'created suite' }
       expect(Suite.find_by(name: 'created suite')).to be
     end
 
     it '400s when creating a nameless suite' do
-      post '/three3/suites', params: { name: 'created suite' }
+      post '/threAS3/suites', params: { name: 'created suite' }
       expect(Suite.find_by(name: 'created suite')).to be
     end
 
     it 'can delete a suite' do
       id = Suite.create(name: 'delete me').id
-      delete "/three3/suites/#{id}"
+      delete "/threAS3/suites/#{id}"
       expect(Suite.where(id: id)).to eq([])
     end
 
     it '400s deleting a suite with in a non-integer id' do
-      delete '/three3/suites/hello'
+      delete '/threAS3/suites/hello'
       expect(response).to have_http_status(400)
     end
 
     it '400s deleting a suite with a negative id' do
-      delete '/three3/suites/hello'
+      delete '/threAS3/suites/hello'
       expect(response).to have_http_status(400)
     end
 
     it '404s deleting a suite that does not exist' do
-      delete '/three3/suites/5'
+      delete '/threAS3/suites/5'
       expect(response).to have_http_status(404)
     end
   end
