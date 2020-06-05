cdc337052015750d2e9fae8caa47a7e7597db114
Alan Glenn <alanglenn74@gmail.com>
2020-06-05 03:25:16 +0000

    feat: implement step controller

--- backend/app/controllers/steps_controller.rb
+++ backend/app/controllers/steps_controller.rb
@@ -1,10 +1,41 @@
 class StepsController < ApplicationController
   def index
+    steps = Concurrent::Future.execute do
+      Step.limit(
+        limit
+      ).offset(
+        offset
+      ).pluck(:id, :keyword, :text).map do |id, keyword, text|
+        { id: id, keyword: keyword, text: text }
+      end
+    end
+    count = Concurrent::Future.execute { Step.count }
+    render json: { offset: offset, count: count.value, records: steps.value }
   end
 
   def create
+    if params[:text].nil?
+      return render json: {errors: ['missing text']}, status: 400
+    end
+    if params[:keyword].nil?
+      return render json: {errors: ['missing keyword']}, status: 400
+    end
+    if params[:keyword].present?
+      unless ['given', 'when', 'then'].include? params[:keyword].downcase
+        return render json: {errors: ['invalid keyword']}, status: 400
+      end
+    end
+    Step.transaction do
+      cased_keyword = params[:keyword].downcase
+      found = Step.find_by keyword: cased_keyword, text: params[:text]
+      return render json: {id: found.id}, status: 200 unless found.nil?
+      created = Step.create! keyword: cased_keyword, text: params[:text]
+      render json: {id: created.id}, status: 200
+    end
   end
 
   def show
+    step = Step.find(params[:id])
+    render json: {keyword: step.keyword, text: step.text, id: step.id }
   end
 end
--- backend/spec/requests/steps_request_spec.rb
+++ backend/spec/requests/steps_request_spec.rb
@@ -1,5 +1,134 @@
 require 'rails_helper'
 
 RSpec.describe "Steps", type: :request do
+  include RequestHelper
 
+  describe 'when it has 3 suites' do
+    before :all do
+      clear_database
+      post '/threAS3/steps', params: {keyword: 'given', text: 'have step'}
+      post '/threAS3/steps', params: {keyword: 'when', text: 'did step'}
+      post '/threAS3/steps', params: {keyword: 'then', text: 'done step'}
+    end
+
+    it 'will obey an offset' do
+      get '/threAS3/steps', params: { offset: 1 }
+      expect_body(
+        offset: 1,
+        count: 3,
+        records: [
+          {keyword: 'then', text: 'done step'},
+          {keyword: 'given', text: 'have step'},
+        ]
+      )
+    end
+
+    it 'will obey a limit' do
+      get '/threAS3/steps', params: { limit: 2 }
+      expect_body(
+        offset: 0,
+        count: 3,
+        records: [
+          {keyword: 'when', text: 'did step'},
+          {keyword: 'then', text: 'done step'},
+        ]
+      )
+    end
+
+    it 'will obey a limit and offset' do
+      get '/threAS3/steps', params: { limit: 1, offset: 1 }
+      expect_body(
+        offset: 1,
+        count: 3,
+        records: [
+          {keyword: 'then', text: 'done step'},
+        ]
+      )
+    end
+  end
+
+
+  ['given', 'when', 'then'].each do |keyword|
+    it "can add a #{keyword} step" do
+      post '/threAS3/steps', params: {
+        keyword: keyword,
+        text: 'a created text'
+      }
+      id = JSON.parse(response.body)['id']
+      get "/threAS3/steps/#{id}"
+      expect(response.status).to eq 200
+      expect(JSON.parse(response.body)).to eq ({
+        'id' => id,
+        'keyword' => keyword,
+        'text' => 'a created text',
+      })
+    end
+  end
+
+  it 'will downcase keyword' do
+    post '/threAS3/steps', params: {
+      keyword: 'Given',
+      text: 'a created text'
+    }
+    id = JSON.parse(response.body)['id']
+    get "/threAS3/steps/#{id}"
+    expect(response.status).to eq 200
+    expect(JSON.parse(response.body)).to eq ({
+      'id' => id,
+      'keyword' => 'given',
+      'text' => 'a created text',
+    })
+  end
+
+  it 'will silently ignore 403ing posts' do
+    post '/threAS3/steps', params: {
+      keyword: 'given',
+      text: 'a created text'
+    }
+    first_id = JSON.parse(response.body)['id']
+    post '/threAS3/steps', params: {
+      keyword: 'given',
+      text: 'a created text'
+    }
+    expect(response.status).to eq 200
+    second_id = JSON.parse(response.body)['id']
+    expect(second_id).to eq first_id
+    get "/threAS3/steps/#{second_id}"
+    expect(JSON.parse(response.body)).to eq ({
+      'id' => second_id,
+      'keyword' => 'given',
+      'text' => 'a created text',
+    })
+  end
+
+  it 'will not accept an invalid keyword' do
+    post '/threAS3/steps', params: {
+      keyword: 'gave',
+      text: 'a created text'
+    }
+    expect(response.status).to eq 400
+    expect(JSON.parse(response.body)).to eq ({
+      'errors' => ['invalid keyword']
+    })
+  end
+
+  it 'will not accept a missing keyword' do
+    post '/threAS3/steps', params: {
+      text: 'a created text'
+    }
+    expect(response.status).to eq 400
+    expect(JSON.parse(response.body)).to eq ({
+      'errors' => ['missing keyword']
+    })
+  end
+
+  it 'will not accept missing text' do
+    post '/threAS3/steps', params: {
+      keyword: 'gave',
+    }
+    expect(response.status).to eq 400
+    expect(JSON.parse(response.body)).to eq ({
+      'errors' => ['missing text']
+    })
+  end
 end
